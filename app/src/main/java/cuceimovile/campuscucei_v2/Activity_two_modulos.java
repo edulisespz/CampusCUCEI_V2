c2d3b2558dc1fc8ad0297442020340638247883f
edulisespz <ed_ulisespz@hotmail.com>
2015-09-11 11:02:04 -0500

    added list Modu
    update in list

--- app/src/main/java/cuceimovile/campuscucei_v2/Activity_two_modulos.java
+++ app/src/main/java/cuceimovile/campuscucei_v2/Activity_two_modulos.java
@@ -56,7 +56,9 @@ public class Activity_two_modulos extends AppCompatActivity {
 
 
     public void loadListView(){
-        String[] listaMod = {"Modulo A","Modulo B"};
+        String[] listaMod = {"Modulo A","Modulo AB","Modulo D","Modulo E","Modulo F","Modulo G","Modulo H","Modulo I","Modulo J"
+                ,"Modulo K","Modulo L","Modulo M","Modulo N","Modulo O","Modulo Otro","Modulo P"
+                ,"Modulo Q","Modulo R","Modulo S","Modulo T","Modulo U","Modulo V","Modulo W","Modulo X","Modulo Z"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.modulos_list_item,listaMod);
 
@@ -86,14 +88,80 @@ public class Activity_two_modulos extends AppCompatActivity {
 
             switch (PositionID){
                 case 0:
-
                     webView.loadUrl("file:///android_asset/moduloA.html");
-
                     break;
                 case 1:
                     webView.loadUrl("file:///android_asset/moduloAB.html");
                     break;
-
+                case 2:
+                    webView.loadUrl("file:///android_asset/moduloD.html");
+                    break;
+                case 3:
+                    webView.loadUrl("file:///android_asset/moduloE.html");
+                    break;
+                case 4:
+                    webView.loadUrl("file:///android_asset/moduloF.html");
+                    break;
+                case 5:
+                    webView.loadUrl("file:///android_asset/moduloG.html");
+                    break;
+                case 6:
+                    webView.loadUrl("file:///android_asset/moduloH.html");
+                    break;
+                case 7:
+                    webView.loadUrl("file:///android_asset/moduloI.html");
+                    break;
+                case 8:
+                    webView.loadUrl("file:///android_asset/moduloJ.html");
+                    break;
+                case 9:
+                    webView.loadUrl("file:///android_asset/moduloK.html");
+                    break;
+                case 10:
+                    webView.loadUrl("file:///android_asset/moduloL.html");
+                    break;
+                case 11:
+                    webView.loadUrl("file:///android_asset/moduloM.html");
+                    break;
+                case 12:
+                    webView.loadUrl("file:///android_asset/moduloN.html");
+                    break;
+                case 13:
+                    webView.loadUrl("file:///android_asset/moduloO.html");
+                    break;
+                case 14:
+                    webView.loadUrl("file:///android_asset/moduloOtro.html");
+                    break;
+                case 15:
+                    webView.loadUrl("file:///android_asset/moduloP.html");
+                    break;
+                case 16:
+                    webView.loadUrl("file:///android_asset/moduloQ.html");
+                    break;
+                case 17:
+                    webView.loadUrl("file:///android_asset/moduloR.html");
+                    break;
+                case 18:
+                    webView.loadUrl("file:///android_asset/moduloS.html");
+                    break;
+                case 19:
+                    webView.loadUrl("file:///android_asset/moduloT.html");
+                    break;
+                case 20:
+                    webView.loadUrl("file:///android_asset/moduloU.html");
+                    break;
+                case 21:
+                    webView.loadUrl("file:///android_asset/moduloV.html");
+                    break;
+                case 22:
+                    webView.loadUrl("file:///android_asset/moduloW.html");
+                    break;
+                case 23:
+                    webView.loadUrl("file:///android_asset/moduloX.html");
+                    break;
+                case 24:
+                    webView.loadUrl("file:///android_asset/moduloZ.html");
+                    break;
             }
 
 
